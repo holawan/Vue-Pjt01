e2ad0d3dd78e69ae29d4fd5a3b260497002ff7e1
holawan <asdf134652@gmail.com>
2022-05-13 15:38:58 +0900

    [update] Moviecard components

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,16 +1,54 @@
 <template>
   <div>
+    <!-- {{movie.title}}
+    {{movie.overview}}
+    <img :src="url" alt="">
+    <hr> -->
+
+    <b-card
+    :title= "movie.title"
+    :img-src= url
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem; max-height: 30rem;" 
+    class="mb-2"
+  >
+    <b-card-text class="text">
+      "{{movie.overview}}"
+    </b-card-text>
+
+    </b-card>
   </div>
 </template>
 
 <script>
+
 export default {
   name:'MovieCard',
-  // props:{
-  // }
+  props:{
+    movie:Object,
+  }
+  ,
+  computed:{
+    url () {
+      return 'https://image.tmdb.org/t/p/w300'+this.movie.poster_path
+    }
+  }
 }
 </script>
 
 <style>
+.text{
+  text-overflow: ellipsis;
+  /* white-space: nowrap; */
+  /* word-break:break-all; */
+
+  width: 20rem; 
+  height: 30rem;
+
+
+}
+
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="home">
-    <movie-card></movie-card>
+  <div class="home container ">
+    <movie-card class="col-3"
+    v-for="movie in movies" :key="movie.id" :movie="movie"
+    ></movie-card>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import MovieCard from '@/components/MovieCard.vue'
-
+import { mapState } from 'vuex'
 
 export default {
   name: 'HomeView',
@@ -16,6 +18,17 @@ export default {
   },
   created(){  
     this.$store.dispatch('getMovie')
+  },
+  computed:{
+    ...mapState(['movies'])
   }
 }
 </script>
+
+<style>
+.home{
+  display: flex;
+
+}
+
+</style>
